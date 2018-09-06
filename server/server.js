66cef6f93e8814a26be8e330c6c1c2269461c838
Tzikas <niko.tzikas@gmail.com>
2018-09-05 20:22:22 -0400

    gogo

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const morgan = require('morgan');
 const passport = require('passport');
 const jsonParser = bodyParser.json();
 
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const sio = require('socket.io');
+const favicon = require('serve-favicon');
+const compression = require('compression');
+
+
+
 const {router: Router, basicStrategy, jwtStrategy} = require('./controllers/router');
 
 mongoose.Promise = global.Promise;
@@ -16,7 +26,7 @@ const app = express();
 
 // Logging
 app.use(morgan('common'));
-
+app.use(compression());
 // CORS
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -72,6 +82,52 @@ function runServer() {
                     mongoose.disconnect();
                     reject(err);
                 });
+var io = sio(server);
+app.disable('x-powered-by');
+io.sockets.on('connection', socket => {
+  console.log('connection ')
+  let room = '';
+  const create = err => {
+    if (err) {
+      return console.log(err);
+    }
+    socket.join(room);
+    socket.emit('create');
+  };
+  // sending to all clients in the room (channel) except sender
+  socket.on('message', message => socket.broadcast.to(room).emit('message', message));
+  socket.on('find', () => {
+    const url = socket.request.headers.referer.split('/');
+    room = url[url.length - 1];
+    const sr = io.sockets.adapter.rooms[room];
+    if (sr === undefined) {
+      // no room with such name is found so create it
+      socket.join(room);
+      socket.emit('create');
+    } else if (sr.length === 1) {
+      socket.emit('join');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+  socket.on('auth', data => {
+    data.sid = socket.id;
+    // sending to all clients in the room (channel) except sender
+    socket.broadcast.to(room).emit('approve', data);
+  });
+  socket.on('accept', id => {
+    io.sockets.connected[id].join(room);
+    // sending to all clients in 'game' room(channel), include sender
+    io.in(room).emit('bridge');
+  });
+  socket.on('reject', () => socket.emit('full'));
+  socket.on('leave', () => {
+    // sending to all clients in the room (channel) except sender
+    socket.broadcast.to(room).emit('hangup');
+    socket.leave(room);});
+});
+
+                
         });
     });
 }
@@ -93,5 +149,15 @@ function closeServer() {
 if (require.main === module) {
     runServer().catch(err => console.error(err));
 }
+
+
+
+
+//const io = require('socket.io')(server); socketEvents(io);
+
+//http.createServer(app).listen(PORT);
+
+// Switch off the default 'X-Powered-By: Express' header
+
 
 module.exports = {app, runServer, closeServer};
